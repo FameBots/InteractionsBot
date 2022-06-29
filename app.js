9ac1d057a20baa8e082a3bd754c11e0ccd1b179b
Sean <sattler.junior@gmail.com>
2022-06-29 14:42:13 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const { Collection } = require('discord.js');
+const { setTimeout } = require('node:timers/promises');
+const {
+  InteractionType,
+  InteractionResponseType,
+  InteractionResponseFlags,
+  MessageComponentTypes,
+  ButtonStyleTypes,
+  verifyKeyMiddleware,
+} = require('discord-interactions');
+
+const Client = {};
+Client.capitalize = async (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+Client.commands = new Collection();
+Client.buttons = new Collection();
+Client.modals = new Collection();
+Client.wait = setTimeout;
+
+Client.getOption = (interaction, option) => {
+  return interaction.data.options[0].options.filter(boo => boo.name === option)[0];
+}
+Client.getSubCommandName = (interaction) => {
+  return interaction.data.options[0].name;
+}
+
+
+// Create an express app
+const app = express();
+
+require("./database/mongoose.js");
+require("./commands.js")(Client);
+
+app.post('/interactions', verifyKeyMiddleware('SECRET'), (req, res) => {
+  const startAt = Date.now();
+  const interaction = req.body;
+  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+    const cmd = Client.commands.get(interaction.data.name);
+
+    console.log(interaction)
+
+    if (!cmd){
+      res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `Can't find this Command. :(`,
+          flags: InteractionResponseFlags.EPHEMERAL,
+        },
+      });
+    }
+
+
+    cmd.run(Client, interaction, res, startAt)
+  } else if(interaction.type === InteractionType.MODAL_SUBMIT) {
+    const modal = Client.modals.get(interaction.data.name);
+    if(!modal) return;
+    console.log(interaction)
+
+    modal.run(Client, interaction, res, startAt);
+  } else if(interaction.type === InteractionType.MESSAGE_COMPONENT) {
+    const button = Client.buttons.get(interaction.data.name);
+    if(!button) return;
+    console.log(interaction)
+
+    button.run(Client, interaction, res, startAt);
+  }
+});
+
+app.listen(6564, () => {
+  console.log('Listening on port 6564');
+});
+
+module.exports = app;
