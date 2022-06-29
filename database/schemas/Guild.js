eb4dbb9578fbd9b3822a4176b4e4119907462bb9
Sean <sattler.junior@gmail.com>
2022-06-29 14:46:06 +0200

    Create Guild.js

---
+++ database/schemas/Guild.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+
+const guildConfigSchema = mongoose.Schema({
+  guildId: {
+    type: mongoose.SchemaTypes.String,
+    required: true,
+    unique: true
+  },
+});
+
+module.exports = mongoose.model(`guildDatabase`, guildConfigSchema);
