fc0515fe6f1526aba36bb904a8c4786aae583f3f
Sean <sattler.junior@gmail.com>
2022-06-29 14:44:50 +0200

    Create ping.js

---
+++ SlashCommands/ping.js
@@ -0,0 +1,30 @@
+const {
+    MessageButton,
+    MessageActionRow,
+    MessageEmbed,
+    MessageAttachment
+} = require("discord.js");
+const {
+    InteractionType,
+    InteractionResponseType,
+    InteractionResponseFlags,
+    MessageComponentTypes,
+    ButtonStyleTypes,
+    verifyKeyMiddleware,
+  } = require('discord-interactions');
+
+
+module.exports = {
+    name: "ping",
+    description: "Get the current bot ping.",
+    cooldown: 5,
+    run: async (client, interaction, res, startAt) => {
+        res.send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+              content: `Pong! :) **${Date.now()-startAt}ms**`,
+              flags: InteractionResponseFlags.EPHEMERAL,
+            },
+          });
+    },
+};
